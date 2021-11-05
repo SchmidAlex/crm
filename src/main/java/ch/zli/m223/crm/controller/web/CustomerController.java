de299f197d3df743e4c063d40d353565ec60e42d
defkon98 <alex_schmid@hotmail.ch>
2021-11-05 15:35:17 +0100

    Comment some code

--- src/main/java/ch/zli/m223/crm/controller/web/CustomerController.java
+++ src/main/java/ch/zli/m223/crm/controller/web/CustomerController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ch.zli.m223.crm.model.Customer;
 import ch.zli.m223.crm.service.CustomerService;
 
+/**
+ * Das ist der Webcontroller für den Customer
+ */
 @Controller
 @RequestMapping("/web/customers")
 public class CustomerController {
@@ -21,6 +24,11 @@ public class CustomerController {
 	@Autowired
 	CustomerService customerService;
 	
+	/**
+	 * Is to get and see all customers
+	 * @param model black magic of spring
+	 * @return to the template customerList.html
+	 */
 	@GetMapping("")
 	public String getAllCustomers(Model model) {
 		List<Customer> customers = customerService.getAllCustomers();
@@ -28,6 +36,12 @@ public class CustomerController {
 		return "customerList";
 	}
 	
+	/**
+	 * Is to see one customer (single-view)
+	 * @param id of the customer
+	 * @param model black magic of spring
+	 * @return to the template customer.html
+	 */
 	@GetMapping("/{id}")
 	public String getOneCustomerById(@PathVariable("id") long id, Model model) {
 		Customer customer = customerService.getCustomerById(id);
@@ -35,17 +49,36 @@ public class CustomerController {
 		return "customer";
 	}
 	
+	/**
+	 * Is to delete a customer
+	 * @param id of the customer
+	 * @param model black magic of spring
+	 * @return lets the function getAllCustomers(Model model) run
+	 */
 	@GetMapping("/{id}/delete")
 	public String deleteCustomer(@PathVariable("id") long id, Model model) {
 		customerService.deleteById(id);
 		return getAllCustomers(model);
 	}
 	
+	/**
+	 * shows the form to add an customer
+	 * @return the view addCustomerForm
+	 */
 	@GetMapping("/addcustomerform")
 	public String addAnCustomerForm() {
 		return "addCustomerForm";
 	}
 	
+	/**
+	 * adds a new customer and shows all again
+	 * @param model black magic of spring
+	 * @param name of the customer
+	 * @param street of the customer
+	 * @param city of the customer
+	 * @param memo from the customer
+	 * @return the new view of all customers (bug with memo)
+	 */
 	@PostMapping("/addcustomer")
 	public String addAnUser(Model model, @RequestParam String name, @RequestParam String street, @RequestParam String city, @RequestParam String memo) {
 		Customer customer = customerService.addCustomer(name, street, city);
--- src/main/java/ch/zli/m223/crm/controller/web/UserController.java
+++ src/main/java/ch/zli/m223/crm/controller/web/UserController.java
@@ -14,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ch.zli.m223.crm.model.AppUser;
 import ch.zli.m223.crm.service.UserService;
 
+/**
+ *  Das ist der Webcontroller für den User
+ * @author User
+ *
+ */
 @Controller
 @RequestMapping("/web/users")
 public class UserController {
@@ -21,6 +26,11 @@ public class UserController {
 	@Autowired
 	UserService userService;
 	
+	/**
+	 * Is to get and see all users
+	 * @param model black magic of spring
+	 * @return the template userList.html
+	 */
 	@GetMapping("")
 	public String getAllUsers(Model model) {
 		List<AppUser> users = userService.getAllUsers();
@@ -28,6 +38,12 @@ public class UserController {
 		return "userList";
 	}
 	
+	/**
+	 * Is to see one user (single-view)
+	 * @param id of the user
+	 * @param model black magic of spring
+	 * @return to the template user.html
+	 */
 	@GetMapping("/{id}")
 	public String getOneUserById(@PathVariable("id") long id, Model model) {
 		AppUser user = userService.getUserById(id);
@@ -35,17 +51,34 @@ public class UserController {
 		return "user";
 	}
 	
+	/**
+	 * Is to delete a user
+	 * @param id of the user
+	 * @param model black magic of spring
+	 * @return lets the function getAllUsers(Model model) run
+	 */
 	@GetMapping("/{id}/delete")
 	public String deleteUser(@PathVariable("id") long id, Model model) {
 		userService.deleteById(id);
 		return getAllUsers(model);
 	}
 	
+	/**
+	 * shows the form to add an user
+	 * @return the view addUserForm
+	 */
 	@GetMapping("/adduserform")
 	public String addAnUserForm() {
 		return "addUserForm";
 	}
 	
+	/**
+	 * adds a new user and shows all again
+	 * @param model black magic of spring
+	 * @param email of the new user
+	 * @param password of the new user
+	 * @return the new view of all users
+	 */
 	@PostMapping("/adduser")
 	public String addAnUser(Model model, @RequestParam String email, @RequestParam String password) {
 		userService.addUser(email, password);
